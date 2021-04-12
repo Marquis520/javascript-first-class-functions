577916061a61843afd004e784a82660aa5c52774
Marquis520 <Marquis520@icloud.com>
2021-04-11 20:00:35 -0400

    Done

---
+++ index.js
@@ -0,0 +1,11 @@
+function morningRoutine(exercise) {
+    let breakfast;
+
+    if (exercise ===liftWeights) {
+        breakfast = 'protein bar';
+    } else if (exercise ===swimFortyLaps) {
+        breakfast = 'kale smoothie';
+    } else {
+        breakfast = 'granola';
+    }
+}
